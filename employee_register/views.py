5ee4c85cd2a7d48c4f939e06ac4f992d18461745
Josephat <sangwajozaphat@gmail.com>
2025-03-24 20:43:53 +0200

    Added insertion operation

--- employee_register/views.py
+++ employee_register/views.py
@@ -1,10 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import EmployeeForm
 # Create your views here.
 def employee_list(request):
     return render(request, 'employee_register/employee_list.html')
 def employee_form(request):
-    form = EmployeeForm()
-    return render(request, 'employee_register/employee_form.html',{'form': form})
+    if request.method == 'GET':
+        form = EmployeeForm()
+        return render(request, 'employee_register/employee_form.html',{'form': form})
+    else:
+        form = EmployeeForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect('/employee/list')
 def employee_delete(request):
     pass
